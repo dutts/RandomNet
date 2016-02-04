126a39d27daee6262431e0615f6561fba892cc46
Richard D <richard@dutton.me.uk>
2016-02-04 19:07:31 +0000

    More nuget supporting stuff

---
+++ RandomNet/NuGet/pack.ps1
@@ -0,0 +1,12 @@
+﻿$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\RandomNet\bin\Release\RandomNet.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\NuGet\RandomNet.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\nuget\RandomNet.compiled.nuspec
+
+& $root\NuGet\NuGet.exe pack $root\nuget\RandomNet.compiled.nuspec
