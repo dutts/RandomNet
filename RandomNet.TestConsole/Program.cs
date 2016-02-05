9be6f72c7de32bf40a9b01fa96d037536bf47ad0
Richard D <richard@dutton.me.uk>
2016-02-05 17:36:11 +0000

    Noddy console test for sequence API call

--- RandomNet.TestConsole/Program.cs
+++ RandomNet.TestConsole/Program.cs
@@ -6,7 +6,13 @@ namespace RandomNet.TestConsole
     {
         public static void Main(string[] args)
         {
-            var result = RandomOrg.IntegerGenerator(10, 1, 6);
+            var result = IntegerGenerator.GenerateAsync(10, 1, 6).Result;
+            foreach (var item in result) Console.WriteLine(item.ToString());
+
+            Console.WriteLine("---");
+
+
+            result = SequenceGenerator.GenerateAsync(1, 6).Result;
             foreach (var item in result) Console.WriteLine(item.ToString());
         }
     }
