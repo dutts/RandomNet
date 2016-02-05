9475fdc379ae322c8a008173090b1a8398439481
Richard D <richard@dutton.me.uk>
2016-02-05 17:35:05 +0000

    Renamed and tidied up IntegerGenerator, added parameter validation. Fixed bug in result processing.

--- RandomNet/IntegerGenerator.cs
+++ RandomNet/IntegerGenerator.cs
@@ -3,35 +3,21 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using RandomNet.Exceptions;
 
 namespace RandomNet
 {
-    public static class RandomOrg
+    public static class IntegerGenerator
     {
-        public static IEnumerable<int> IntegerGenerator(int numberOfInts, int minValue, int maxValue)
+        public static async Task<IEnumerable<int>> GenerateAsync(int numberOfInts, int minValue, int maxValue)
         {
+            if (minValue == maxValue) return new List<int> { minValue };
+            if (maxValue < minValue) throw new ArgumentException("max cannot be less than min");
+            if (numberOfInts < 1 || numberOfInts > Math.Pow(10,3)) throw new ArgumentException("Number of integers requested must be between 1 and 1e4", nameof(numberOfInts));
+            if (minValue < -Math.Pow(10,8) || minValue > Math.Pow(10,8)) throw new ArgumentException("Value of min must be between -1e9 and 1e9", nameof(minValue));
+            if (maxValue < -Math.Pow(10,8) || maxValue > Math.Pow(10,8)) throw new ArgumentException("Value of max must be between -1e9 and 1e9", nameof(maxValue));
+
             var uri = $"http://www.random.org/integers/?num={numberOfInts}&min={minValue}&max={maxValue}&col=1&base=10&format=plain&rnd=new";
-
-            try
-            {
-                using (var response = Request.Create(uri).GetResponse())
-                using (var dataStream = response.GetResponseStream())
-                using (var reader = new StreamReader(dataStream))
-                {
-                    var responseFromServer = reader.ReadToEnd();
-                    var numbers = responseFromServer.Split('\n');
-                    return numbers.Where(n => n.Length > 0).Select(int.Parse);
-                }
-            }
-            catch (Exception e)
-            {
-                throw new ConnectionException("Unable to connect to Random.org.", e);
-            }
-        }
-
-        public static async Task<IEnumerable<int>> IntegerGeneratorAsync(int numberOfInts, int minValue, int maxValue)
-        {
-            var uri = $"http://www.random.org/integers/?num={numberOfInts}&min={minValue}&max={maxValue}";
 
             try
             {
@@ -42,7 +28,7 @@ namespace RandomNet
                 {
                     var responseFromServer = reader.ReadToEnd();
                     response.Dispose();
-                    return responseFromServer.Split('\n').Select(int.Parse);
+                    return responseFromServer.Split('\n').Where(n => n.Length > 0).Select(int.Parse);
                 }
             }
             catch (Exception e)
